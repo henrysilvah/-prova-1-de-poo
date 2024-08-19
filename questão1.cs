840b0e4ade84ea33209da2c4a6f7775750141a20
henrysilvah <silva.felipe2@escolar.ifrn.edu.br>
2024-08-19 17:39:10 -0300

    Create questão1.cs

---
+++ questão1.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+       
+        int numeroInteiro = 42;
+        float numeroDecimal = 3.14f;
+        string texto = "Olá, Mundo!";
+        bool verdadeiroOuFalso = true;
+
+
+       
+        Console.WriteLine("isto é um numero inteiro: " + numeroInteiro);
+        Console.WriteLine("isto é um numero decimal: " + numeroDecimal);
+        Console.WriteLine("isto é um texto: " + texto);
+        Console.WriteLine("isto é uma variavel do tipo booleano: " + verdadeiroOuFalso);
+
+
+       
+    }
+}
