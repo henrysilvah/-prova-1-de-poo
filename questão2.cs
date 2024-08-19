125f53d70587fba14434807a1255e99a2613c0ca
henrysilvah <silva.felipe2@escolar.ifrn.edu.br>
2024-08-19 17:41:28 -0300

    Create questão2.cs

---
+++ questão2.cs
@@ -0,0 +1,9 @@
+double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double soma = a + b;
+        double subtracao = a - b;
+        double multiplicacao = a * b;
+        double divisao = a / b;
+
+
+ Console.WriteLine($"Soma = {soma} Subtração = {subtracao} Multiplicação = {multiplicacao} Divisão = {divisao}");
